58a2b597aceb2c60969bb67415107da04bf1a734
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-22 16:35:44 +0200

    IDEA for solving 11 problem

---
+++ conteiner_with_most_water/11.py
@@ -0,0 +1,12 @@
+# IDEA
+# We can use two pointers to solve this problem
+# We need to set two pointers, one on the left and one on the right of the array
+# Then we can calculate the area between those two pointers and actualize the maximum area
+# Then we can move the pointer with the smaller height to the right or left depending on the height
+# and actualize the maximum area if the new area is greater than the biggest one
+# TIME COMPLEXITY
+# The time complexity is O(n) because we only need to iterate the array once
+# SPACE COMPLEXITY
+# The space complexity is O(1) because we only need to use two pointers to solve this problem
+# and constant number of variables
+
