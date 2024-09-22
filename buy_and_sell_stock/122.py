1106727c2eb752e8c01552b8ce9de303313b9bed
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-22 10:16:41 +0200

    Idea for solving the problem 122

---
+++ buy_and_sell_stock/122.py
@@ -0,0 +1,10 @@
+# IDEA
+# we can handle only one stock at a time, but we can sell it the same day we buy it
+# we want to sell stock always when we can make a profit and actualize stock we handle
+# buy one we are buying or actualizing stock we handle with the lowest price we can
+# TIME COMPLEXITY
+# the time complexity is O(n) because we only need to iterate the array once
+# SPACE COMPLEXITY
+# the space complexity is O(1)
+
+
