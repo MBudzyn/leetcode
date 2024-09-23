1721d8baf5234270745f7394fc71d2f1a920c35f
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-23 11:54:01 +0200

    solution of the 2 problem

--- 2/2.py
+++ 2/2.py
@@ -9,3 +9,46 @@
 # The time complexity is O(n) because we only need to iterate the lists once
 # SPACE COMPLEXITY
 # The space complexity is O(n) because we need to store the result list
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        res = ListNode()
+        head = res
+        carry = 0
+        while l1 is not None and l2 is not None:
+            sum = l1.val + l2.val + carry
+            carry = sum // 10
+            res.val = sum % 10
+            l1 = l1.next
+            l2 = l2.next
+            if l1 is not None and l2 is not None:
+                res.next = ListNode()
+                res = res.next
+
+        if l1 is not None:
+            while l1 is not None:
+                sum = l1.val + carry
+                carry = sum // 10
+                res.next = ListNode(sum % 10)
+                res = res.next
+                l1 = l1.next
+
+        if l2 is not None:
+            while l2 is not None:
+                sum = l2.val + carry
+                carry = sum // 10
+                res.next = ListNode(sum % 10)
+                res = res.next
+                l2 = l2.next
+
+        if carry > 0:
+            res.next = ListNode(carry)
+
+        return head
