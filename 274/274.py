d953497bebb1b843ba93de5c0ffab4fc6275ce5f
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-24 09:27:15 +0200

    IDEA for problem 274

---
+++ 274/274.py
@@ -0,0 +1,8 @@
+# IDEA
+# becouse of the definition of h-index first idea that comes to mind is to sort the array and then iterate through it
+# and check if the current element is greater than the number of elements that are greater than it
+# if so we can actualize the cd result and return it at the end if we didn't fing greater h-index
+# TIME COMPLEXITY
+# The time complexity is O(nlogn) because we need to sort the array
+# SPACE COMPLEXITY
+# The space complexity is O(1) because we only need to use constant number of variables
