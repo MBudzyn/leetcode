bc54c4280bcd202df9162201bbfd847db9279f70
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-25 11:14:38 +0200

    problem 36 solved

--- 36/36.py
+++ 36/36.py
@@ -8,3 +8,44 @@
 # SPACE COMPLEXITY
 # The space complexity is O(1) because we only need to use constant number of variables
 
+from typing import List
+
+
+def dot_ignore(t):
+    return [num for num in t if num != "."]
+
+
+def get_column(t_2, c_ind):
+    return [row[c_ind] for row in t_2]
+
+
+def get_row(t_2, r_ind):
+    return t_2[r_ind]
+
+
+def parse_to_one_dim(data):
+    return [num for row in data for num in row]
+
+
+def get_square(t_2, r_ind, c_ind):
+    return parse_to_one_dim([row[c_ind:c_ind + 3] for row in t_2[r_ind:r_ind + 3]])
+
+
+def check_corr(t):
+    t = dot_ignore(t)
+    return len(t) == len(list(set(t)))
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in [0, 3, 6]:
+            for j in [0, 3, 6]:
+                if not check_corr(get_square(board, i, j)):
+                    return False
+        for i in range(9):
+            if not check_corr(get_row(board, i)):
+                return False
+        for i in range(9):
+            if not check_corr(get_column(board, i)):
+                return False
+        return True
