ce0e1ffa8d0d811f40bc66bf18584df88e8a9ebf
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-25 11:12:56 +0200

    Idea for the 36 problem

---
+++ 36/36.py
@@ -0,0 +1,10 @@
+# IDEA
+# to check if the sudoku is valid we need to check all rows, columns and 3x3 squares
+# and return False if we find any duplicates or True if we don't
+# To apply this idea we will create three helper functions to get rows, columns and 3x3 squares
+# then we will iterate through all of them and check if there are any duplicates
+# TIME COMPLEXITY
+# The time complexity is O(1) because the board is always 9x9
+# SPACE COMPLEXITY
+# The space complexity is O(1) because we only need to use constant number of variables
+
