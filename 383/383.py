4f7b43ce2e6a9a6f15eaf8fe1566d3cbc4032195
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-25 11:24:58 +0200

    problem 383 solved

--- 383/383.py
+++ 383/383.py
@@ -10,3 +10,22 @@
 # SPACE COMPLEXITY
 # The space complexity is O(n) because in the worst case we will store all the letters from the first string
 # with their counts
+
+def construct_dict_ct(s: str) -> dict:
+    dct = {}
+    for c in s:
+        if c in dct:
+            dct[c] += 1
+        else:
+            dct[c] = 1
+    return dct
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        dict_r = construct_dict_ct(ransomNote)
+        dict_m = construct_dict_ct(magazine)
+        for key in dict_r.keys():
+            if key not in dict_m or dict_r[key] > dict_m[key]:
+                return False
+        return True
