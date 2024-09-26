0131b254801298b0597187892280732a5aeaf6e8
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-26 13:01:54 +0200

    IDEA for solving the 19 problem

---
+++ 19/19.py
@@ -0,0 +1,11 @@
+# IDEA
+# we can solve this problem by using two pointers,
+# one pointer will by in 0 position, and the other we will move two n position
+# then we will move two pointers at the same time, until the second pointer reaches the end
+# then we will remove the node that is the next node of the first pointer
+# TIME COMPLEXITY
+# O(N) because we iterate through the list
+# SPACE COMPLEXITY
+# O(1) because we don't use any extra space
+
+
