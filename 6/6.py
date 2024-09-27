c8d361ff1d379ef7453b0b4127141f73ab10b3b4
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-27 11:40:24 +0200

    IDEA for solving problem 6

---
+++ 6/6.py
@@ -0,0 +1,12 @@
+# IDEA
+# to solve this problem, we can create 2D array that on each lvl will store
+# the letters of the layer of the zigzag pattern of the string
+# then we will iterate through the word and by changing direction we will by
+# adding the letters to the array on the correct level
+# then we will iterate through the array and concatenate the letters
+# TIME COMPLEXITY
+# O(N) because we iterate through the word
+# SPACE COMPLEXITY
+# O(N) because we store the letters in the 2D array
+
+
