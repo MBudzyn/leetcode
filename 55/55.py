883e842ef2cc45bedcb9203a61d9ebb4e00c6da6
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-28 14:01:18 +0200

    IDEA for solving the 55 problem

---
+++ 55/55.py
@@ -0,0 +1,11 @@
+# IDEA
+# we can solve this problem by using greedy algorithm
+# we will iterate through the array and for each position we will calculate
+# the max size of jump we can make from that position in the array
+# as the max from the current position and the previous max - 1
+# then if we can reach the end of the array, we will return True
+# in other case we will return False
+# TIME COMPLEXITY
+# O(N) because we iterate through the array
+# SPACE COMPLEXITY
+
