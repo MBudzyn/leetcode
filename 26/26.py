6857bbc4a08e30d71f71104c6f52c7e314a350de
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-29 21:24:30 +0200

    IDEA for solving 26 problem

---
+++ 26/26.py
@@ -0,0 +1,7 @@
+# IDEA
+# we can solve this problem by simply deleting the duplicates
+# iterating through the list from the second element to the end
+# if the current element is equal to the previous element, we delete it
+# we return the length of the list after deleting the duplicates
+# Time complexity: O(n)
+# Space complexity: O(1)
