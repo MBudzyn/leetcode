79b5f684c54cd698e46862ecbd1bfbbc077d73d7
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-29 21:15:07 +0200

    idea for solving the 71 problem

---
+++ 71/71.py
@@ -0,0 +1,11 @@
+# IDEA
+# we can solve this problem by implementing stack as array
+# first put the all non-Null elements to the stack
+# then delete all single "." symbols and iterate through the stack
+# by deleting the last element if the element is equal to ".."
+# TIME COMPLEXITY
+# O(N) because we iterate through the string
+# SPACE COMPLEXITY
+# O(N) because we use stack to store the elements
+
+
