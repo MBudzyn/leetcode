49a7f42c6ca22c492a60d451a81aff934c6f3f1e
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-29 21:25:22 +0200

    problem solved

--- 26/26.py
+++ 26/26.py
@@ -5,3 +5,14 @@
 # we return the length of the list after deleting the duplicates
 # Time complexity: O(n)
 # Space complexity: O(1)
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        i = 1
+        while i < len(nums):
+            if nums[i] == nums[i - 1]:
+                nums.pop(i)
+            else:
+                i += 1
+
+        return len(nums)
