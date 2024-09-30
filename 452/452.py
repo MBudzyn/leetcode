eef6fd28f8e6248506931be4f1e26e12fa764dbd
Mateusz Budzyński <budzynekm09@gmail.com>
2024-09-30 11:02:09 +0200

    IDEA for solving the 452 problem

---
+++ 452/452.py
@@ -0,0 +1,9 @@
+# IDEA
+# we can solve this problem by using greedy algorithm
+# we iterate through the list from the second element to the end
+# if the current element[0] is > then previous tmp, we add 1 to the result
+# else we update the previous tmp[1] to the min(tmp, element[1])
+# COMPLEXITY
+# Time: O(n)
+# Space: O(1)
+
