ea6ac437aa14cab3146421c591fdd0b84c7b33bd
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-01 10:32:06 +0200

    Idea for the 80 problem

---
+++ 80/80.py
@@ -0,0 +1,8 @@
+# IDEA
+# because we can store whatever we want at the end of the array,
+# we will be simply skipping the elements that have more than 2 occurrences
+# and rest of the elements will be moved to the left of the array, on
+# the first skipped place, then we will actualize the index to move the elements
+# to the left of the array, and we will return the index of the last "to_insert"
+
+
