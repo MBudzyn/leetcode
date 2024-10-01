627a841fb85df3ddf8f2bda4737f3c4e40c2e883
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-01 10:32:51 +0200

    Problem 80 solved

--- 80/80.py
+++ 80/80.py
@@ -5,4 +5,24 @@
 # the first skipped place, then we will actualize the index to move the elements
 # to the left of the array, and we will return the index of the last "to_insert"
 
+from typing import List
 
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        ct = 0
+        duplicate_ct = 1
+        to_insert = -1
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                duplicate_ct += 1
+                if duplicate_ct == 3 and to_insert == -1:
+                    to_insert = i
+            else:
+                duplicate_ct = 1
+
+            if to_insert != -1 and duplicate_ct <= 2:
+                nums[to_insert] = nums[i]
+                to_insert += 1
+                ct += 1
+
+        return to_insert if to_insert != -1 else len(nums)
