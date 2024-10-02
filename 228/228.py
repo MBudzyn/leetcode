f6e0ae624a5d3a118cea80d9a55cfe4a0c4be9fb
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-02 10:49:21 +0200

    Idea for solving the 228 problem

---
+++ 228/228.py
@@ -0,0 +1,5 @@
+# IDEA
+# at the beginning we will create a help function that will parse the tmp array
+# to correct format, then we will use simple greedy algorithm that will
+# check if the next element have value, exactly previous + 1 and if not
+# we will call pom function to parse correct fragment of the array
