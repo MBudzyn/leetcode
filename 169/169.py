ac98bc03e119d9ffb14900d730f37acd01b564dc
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-03 07:21:00 +0200

    IDEA for solving problem 169

---
+++ 169/169.py
@@ -0,0 +1,12 @@
+# IDEA
+# we can solve this problem by using two main variables, one to store
+# the current candidate to be majority element, and the other to store
+# occurrences of the candidate, if the candidate is the same as the current
+# element, we will increment the count, otherwise we will decrement the count
+# if the count is zero, we will change the candidate to the current element
+# TIME COMPLEXITY
+# O(n), we are iterating over the array once
+# SPACE COMPLEXITY
+# O(1), we are using only constant space
+
+
