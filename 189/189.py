d6b5a74f1ca4d7ea1f71db0cbd22772a877f1e65
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-05 08:40:34 +0200

    IDEA for solving the 189 problem

---
+++ 189/189.py
@@ -0,0 +1,8 @@
+# IDEA
+# we can solve this problem by using simple operations on list to get the
+# first and second part of the list and swap them to get the desired output
+# TIME COMPLEXITY
+# The time complexity is O(n) as we are iterating through the list only once
+# SPACE COMPLEXITY
+# The space complexity is O(1) as we are not using any extra space
+
