d80294a8ca7bd63f7b316c390aae49ec42a012c0
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-05 09:02:45 +0200

    problem solved

--- 86/86.py
+++ 86/86.py
@@ -6,3 +6,47 @@
 # The time complexity is O(n) as we are iterating through the list only once
 # SPACE COMPLEXITY
 # The space complexity is O(1) as we are not using any extra space
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        greater = None
+        less = None
+        greater_head = None
+        less_head = None
+        while head:
+            if head.val < x:
+                if less:
+                    less.next = head
+                    less = less.next
+                else:
+                    less = head
+                    less_head = less
+            else:
+                if greater:
+                    greater.next = head
+                    greater = greater.next
+                else:
+                    greater = head
+                    greater_head = greater
+            head = head.next
+        if greater:
+            greater.next = None
+            if less:
+                less.next = greater_head
+                return less_head
+            else:
+                return greater_head
+        elif less:
+            less.next = None
+            return less_head
+
+        return None
