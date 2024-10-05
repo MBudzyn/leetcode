c16618562a4384a50d8d7672c242db78a786a1d2
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-05 09:01:18 +0200

    IDEA for solving the 86 problem

---
+++ 86/86.py
@@ -0,0 +1,8 @@
+# IDEA
+# we will partition the linked list into two parts, one that will store
+# elements less than x and the other that will store elements greater
+# or equal to x. We will then merge the two lists to get the desired output
+# TIME COMPLEXITY
+# The time complexity is O(n) as we are iterating through the list only once
+# SPACE COMPLEXITY
+# The space complexity is O(1) as we are not using any extra space
