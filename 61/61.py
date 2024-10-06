526f1c695dbacfd852d91bb6c1fa16744307b053
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-06 13:47:35 +0200

    IDEA for the 61 problem

---
+++ 61/61.py
@@ -0,0 +1,6 @@
+# IDEA
+# in first iteration, we need to count number of elements in the linked list
+# ten we need to actualize the value of k to k % n (n is the number of elements in the linked list)
+# then we need to find the element that is n - k - 1, and correctly merge two parts of the linked list
+# Time complexity: O(n)
+# Space complexity: O(1)
