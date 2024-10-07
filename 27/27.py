79a629754544f7854235f4bcae070d264d962bfd
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-07 13:36:59 +0200

    the 27 problem solved

--- 27/27.py
+++ 27/27.py
@@ -4,3 +4,11 @@
 # Time complexity: O(n)
 # Space complexity: O(1)
 
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        ct = 0
+        indeksy = [i for i in range(len(nums)) if nums[i] == val]
+        for i in indeksy:
+            nums.pop(i-ct)
+            ct += 1
+        return len(nums)
