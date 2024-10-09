ef7b813060f8e0a8d854e101ae27130a2d19bbac
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-09 11:39:17 +0200

    IDEA for solving the 100 problem

---
+++ 100/100.py
@@ -0,0 +1,10 @@
+# IDEA
+# Trees are identical if they have the same structure and the same node values at each position.
+# We can use the recursive function to check if the trees are identical.
+# if in any node the values are different, we can return False
+# if we reach the end of the tree, we can return True
+# TIME COMPLEXITY
+# O(n) because we need to visit every node in the tree
+# SPACE COMPLEXITY
+# O(n) because of the recursion stack
+
