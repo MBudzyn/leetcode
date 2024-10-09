ef3616dc493c99381de7e3fc87aff613ce830e25
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-09 12:11:15 +0200

    IDEA for the 226 problem

---
+++ 226/226.py
@@ -0,0 +1,6 @@
+# IDEA
+# we will iterate over the tree using stack, and for every node in the stack we will
+# swap the left and right children, then we will append the left child and right child
+# to the stack if they are not None
+# Time complexity: O(n)
+# Space complexity: O(n)
