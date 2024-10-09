ce98a789ee4cd8980dab52a8d25d6d271a107225
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-09 11:17:57 +0200

    Time and space complexity

--- 104/104.py
+++ 104/104.py
@@ -1,6 +1,11 @@
 # IDEA
 # the max depth of the binary tree is the maximum depth of the left or right subtree + 1
 # we can create a recursive function that will return the maximum depth of the tree
+# TIME COMPLEXITY
+# O(n) because we need to visit every node in the tree
+# SPACE COMPLEXITY
+# O(n) because of the recursion stack
+
 
 from typing import Optional
 
