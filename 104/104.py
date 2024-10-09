101f4fa9b33792f81897c3f5d57698d7d3daa43e
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-09 11:13:37 +0200

    IDEA for solving the 104 problem

---
+++ 104/104.py
@@ -0,0 +1,4 @@
+# IDEA
+# the max depth of the binary tree is the maximum depth of the left or right subtree + 1
+# we can create a recursive function that will return the maximum depth of the tree
+
