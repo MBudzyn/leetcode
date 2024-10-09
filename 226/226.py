e3192cb833dddef964bc6bd4ae162862c79ccda7
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-09 12:13:07 +0200

    the 226 problem solved

--- 226/226.py
+++ 226/226.py
@@ -4,3 +4,30 @@
 # to the stack if they are not None
 # Time complexity: O(n)
 # Space complexity: O(n)
+
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        head = root
+        if head is None:
+            return None
+        stack = [root]
+        while stack:
+            actual = stack.pop(0)
+            tmp_l = actual.left
+            actual.left = actual.right
+            actual.right = tmp_l
+            if actual.left:
+                stack.append(actual.left)
+            if actual.right:
+                stack.append(actual.right)
+        return head
