85ead6b6491c67aa7969962ca9618adeec81c660
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-09 11:16:29 +0200

    the 104 problem solved

--- 104/104.py
+++ 104/104.py
@@ -2,3 +2,21 @@
 # the max depth of the binary tree is the maximum depth of the left or right subtree + 1
 # we can create a recursive function that will return the maximum depth of the tree
 
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        else:
+            return 1 + max(self.maxDepth(root.right), self.maxDepth(root.left))
+
+
