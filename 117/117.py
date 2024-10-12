9f7b05ff8729595aff4e8fe83f4a19212b2355fa
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-12 17:59:32 +0200

    the 117 problem solved

--- 117/117.py
+++ 117/117.py
@@ -8,4 +8,32 @@
 # which is n/2 nodes in the worst case
 
 
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        stack = [root]
+        head = root
+        new_stack = []
+
+        while stack:
+            for i in range(len(stack)):
+                if i + 1 < len(stack):
+                    stack[i].next = stack[i + 1]
+                if stack[i].left:
+                    new_stack.append(stack[i].left)
+                if stack[i].right:
+                    new_stack.append(stack[i].right)
+            stack = new_stack
+            new_stack = []
+        return head
 
