ca39f207f9288ef794a65e1a483d45e48a5080e8
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-13 10:08:01 +0200

    IDEA for the 112 problem

---
+++ 112/112.py
@@ -0,0 +1,7 @@
+# IDEA
+# we can solve this problem by recursion
+# if the root is None the return True if the targetSum is 0
+# else return recursion (root.left, targetSum - root.val) or recursion(root.right, targetSum - root.val)
+# Time complexity: O(n)
+# Space complexity: O(n)
+
