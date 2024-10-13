859cf7b71f898965963285883ac6327ca5859134
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-13 10:11:27 +0200

    IDEA correction and problem solved with all tests passed

--- 112/112.py
+++ 112/112.py
@@ -1,6 +1,8 @@
 # IDEA
 # we can solve this problem by recursion
-# if the root is None the return True if the targetSum is 0
+# if the root is None we need to return False because if there is no root there is no path
+# if the root.left and root.right is None we need to check if the targetSum is equal to the root.val
+# if it is return True, else return False
 # else return recursion (root.left, targetSum - root.val) or recursion(root.right, targetSum - root.val)
 # Time complexity: O(n)
 # Space complexity: O(n)
@@ -18,7 +20,9 @@ class TreeNode:
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if root is None:
-            return targetSum == 0
+            return False
+        elif root.left is None and root.right is None:
+            return targetSum == root.val
         else:
             return (self.hasPathSum(root.left, targetSum - root.val) or
                     self.hasPathSum(root.right, targetSum - root.val))
