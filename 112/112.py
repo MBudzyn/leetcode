ecea2a8a14b47e588170d7b7317517061928975c
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-13 10:09:36 +0200

    solution for the 112 problem

--- 112/112.py
+++ 112/112.py
@@ -5,3 +5,20 @@
 # Time complexity: O(n)
 # Space complexity: O(n)
 
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return targetSum == 0
+        else:
+            return (self.hasPathSum(root.left, targetSum - root.val) or
+                    self.hasPathSum(root.right, targetSum - root.val))
