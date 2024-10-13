d739e322803287cf0d1e52a89cd28fc37ba754d2
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-13 16:04:24 +0200

    solution with all tests on leetcode passed

--- 129/129.py
+++ 129/129.py
@@ -8,3 +8,26 @@
 # SPACE COMPLEXITY
 # O(h) -- h is the height of the tree, the maximum depth of the recursion stack which in worst case is O(n)
 
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        elif root.right is None and root.left is None:
+            return root.val
+        else:
+            if root.right is not None:
+                root.right.val = root.val * 10 + root.right.val
+            if root.left is not None:
+                root.left.val = root.val * 10 + root.left.val
+            return (self.sumNumbers(root.right) +
+                    self.sumNumbers(root.left))
