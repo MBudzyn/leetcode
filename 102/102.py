a92ded914586db1f4e5a0b75667c57718af147aa
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-14 10:43:33 +0200

    IDEA for solving the 102 problem

---
+++ 102/102.py
@@ -0,0 +1,8 @@
+# IDEA
+# we will iterate through the tree using the stack, and we will be adding each level of
+# values to the result list
+# TIME COMPLEXITY
+# O(n) -- we visit every node once
+# SPACE COMPLEXITY
+# O(n) in the worst case, when we store all the nodes of the last layer in the stack
+
