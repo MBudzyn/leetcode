a61a88a8f6f17a1a4d8acc5110c40d096ce721a1
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-15 09:23:57 +0200

    IDEA for the 199 problem

---
+++ 199/199.py
@@ -0,0 +1,7 @@
+# IDEA
+# we can solve this problem by iterating through the tree using stack (BFS)
+# and to the result we can add the value of the node in the last position of
+# actual layer.
+# The time complexity is O(n) where n is the number of nodes in the tree
+# The space complexity is O(n) where n is the number of nodes in the tree
+
