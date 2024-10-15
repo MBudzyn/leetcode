ccf39b3c0b953adafd59d1dd7132e9adf87ee5f3
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-15 09:24:53 +0200

    solution for the 199 problem

--- 199/199.py
+++ 199/199.py
@@ -4,4 +4,29 @@
 # actual layer.
 # The time complexity is O(n) where n is the number of nodes in the tree
 # The space complexity is O(n) where n is the number of nodes in the tree
+from typing import List, Optional
 
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if not root:
+            return []
+        stack = [root]
+        while stack:
+            new_stack = []
+            result.append(stack[-1].val)
+            for node in stack:
+                if node.left:
+                    new_stack.append(node.left)
+                if node.right:
+                    new_stack.append(node.right)
+            stack = new_stack
+        return result
