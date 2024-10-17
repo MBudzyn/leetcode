bf44bd6247dc68c690b8e18f92478a67e0ebb189
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-17 09:04:23 +0200

    solution for the 21 problem

--- 21/21.py
+++ 21/21.py
@@ -6,6 +6,35 @@
 # O(N) because we iterate through the lists
 # SPACE COMPLEXITY
 # O(N) because we use stack to store the elements
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = ListNode()
+        res = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                res.next = list1
+                res = res.next
+                list1 = list1.next
+            else:
+                res.next = list2
+                res = res.next
+                list2 = list2.next
+
+        if list1:
+            res.next = list1
+        if list2:
+            res.next = list2
+
+        return head.next
 
 
 
