3350cd8f90e16471a6867b9df587fdacb2f2c97a
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-17 09:02:21 +0200

    IDEA for solving the 21 problem

---
+++ 21/21.py
@@ -0,0 +1,12 @@
+# IDEA
+# we will iterate through the both lists and compare the first elements
+# then to the result we will add the smaller one and delete it from the list
+# at the end we will add the rest of the non-empty list
+# TIME COMPLEXITY
+# O(N) because we iterate through the lists
+# SPACE COMPLEXITY
+# O(N) because we use stack to store the elements
+
+
+
+
