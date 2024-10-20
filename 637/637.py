c3da66989191693d3d4f35d2ca65ca312decbd46
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-20 19:49:31 +0200

    solution for the 637 problem

--- 637/637.py
+++ 637/637.py
@@ -6,3 +6,31 @@
 # SPACE COMPLEXITY
 # O(n) where n is the number of nodes in the tree
 
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        if not root:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            new_stack = []
+            tmp = 0
+            for node in stack:
+                tmp += node.val
+                if node.left:
+                    new_stack.append(node.left)
+                if node.right:
+                    new_stack.append(node.right)
+            res.append(tmp / len(stack))
+            stack = new_stack
+        return res
