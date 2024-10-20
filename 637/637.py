43691baf076bc2abc7bfead3c79ee07d091431d4
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-20 19:47:20 +0200

    IDEA for solufion for the 637 problem

---
+++ 637/637.py
@@ -0,0 +1,8 @@
+# IDEA
+# we can iterate through the tree using stack (BFS)
+# and for each lvl calculate the average value of the nodes
+# TIME COMPLEXITY
+# O(n) where n is the number of nodes in the tree
+# SPACE COMPLEXITY
+# O(n) where n is the number of nodes in the tree
+
