5a1e30489d0139b62e8bf19402fc60c6ec8c5968
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-21 10:55:35 +0200

    IDEA for the 103 problem

---
+++ 103/103.py
@@ -0,0 +1,6 @@
+# IDEA
+# we can solve this problem by iterating through the tree using stack (BFS)
+# and depending on actual case we will add the lvl in normal order or reversed
+# The time complexity is O(n) where n is the number of nodes in the tree
+# The space complexity is O(n) where n is the number of nodes in the tree
+
