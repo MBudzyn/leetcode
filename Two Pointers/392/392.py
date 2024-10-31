11e3c6a4436ab3794f7d5acf3d037b894cf538bb
Mateusz Budzyński <budzynekm09@gmail.com>
2024-10-31 07:19:01 +0100

    idea for solving the 392 problem

---
+++ Two Pointers/392/392.py
@@ -0,0 +1,11 @@
+# IDEA
+# we can solve this problem by iterating through the string and checking if
+# the current character is equal to actual character we are looking for
+# if it is, we can move the pointer to the right, if not, the pointer will stay in the same place
+# at the end we can check if the pointer is out of the string and return True if it is
+# in other case we can return False
+# TIME COMPLEXITY
+# O(n) because we iterate through the string
+# SPACE COMPLEXITY
+# O(1) because we only use constant number of variables
+
